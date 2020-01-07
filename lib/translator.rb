aeb3511114613dadd47c4a0981fa00158a34c471
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-07 22:00:55 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,29 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+require "pry"
+def load_library(file_path)
+  result = YAML.load_file(file_path)
+  
+  #           "japanese_emoticon":"meaning"   "english_emoticon":japanese_emoticon
+                # "☜(⌒▽⌒)☞" => "angel"        " O:)" => "☜(⌒▽⌒)☞"
+  final_hash = { "get_meaning":{},              "get_emoticon":{} }
+  
+  result.each do |meaning ,emoticon|
+         
+          # binding.pry
+                if final_hash[:get_meaning][emoticon[1]]== nil 
+                  final_hash[:get_meaning][emoticon[1]] =  meaning
+                end
+              
+                if final_hash[:get_emoticon][emoticon[0]] == nil 
+                  final_hash[:get_emoticon][emoticon[0]] = emoticon[1]
+                end 
+    
+  end 
+  final_hash
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
