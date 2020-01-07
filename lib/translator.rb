8b2a8108cdc6b55aabdda54fbb45e1877221eb72
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-07 22:55:28 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,6 @@ def load_library(file_path)
   final_hash = { "get_meaning":{},              "get_emoticon":{} }
   
   result.each do |meaning ,emoticon|
-         
           # binding.pry
                 if final_hash[:get_meaning][emoticon[1]]== nil 
                   final_hash[:get_meaning][emoticon[1]] =  meaning
@@ -18,17 +17,37 @@ def load_library(file_path)
                 if final_hash[:get_emoticon][emoticon[0]] == nil 
                   final_hash[:get_emoticon][emoticon[0]] = emoticon[1]
                 end 
-    
   end 
   final_hash
 end
 
 
+def get_japanese_emoticon(file_path, emoticon)
+  # binding.pry
+  sheet  = load_library(file_path) 
+ 
+  result = sheet[:get_emoticon][emoticon]
 
-def get_japanese_emoticon
-  # code goes here
+  if result == nil 
+      return "Sorry, that emoticon was not found"
+  end
+  
+  return result            
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(file_path, emoticon)
+  sheet = load_library(file_path)
+  result = sheet[:get_meaning][emoticon]
+  
+  if result == nil 
+    return "Sorry, that emoticon was not found"
+  end 
+  
+  return result
+
 end
+
+
+
+
