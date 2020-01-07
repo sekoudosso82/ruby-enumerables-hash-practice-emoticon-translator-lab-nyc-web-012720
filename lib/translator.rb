e85d8db11cfa73b3de96b5efe6725fa9275f8518
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-07 22:58:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,4 +18,17 @@ def load_library(file_path)
                 end 
   end 
   final_hash
+end  
+
+def get_japanese_emoticon(file_path, emoticon)
+  # binding.pry
+  sheet  = load_library(file_path) 
+ 
+  result = sheet[:get_emoticon][emoticon]
+
+  if result == nil 
+      return "Sorry, that emoticon was not found"
+  end
+  
+  return result            
 end
