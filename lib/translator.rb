831d68f91d987ac0eb84b0c9b3c2c4fb7767b121
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-07 22:56:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,6 @@ def load_library(file_path)
   result = YAML.load_file(file_path)
   
   #           "japanese_emoticon":"meaning"   "english_emoticon":japanese_emoticon
-<<<<<<< HEAD
                 # "☜(⌒▽⌒)☞" => "angel"        " O:)" => "☜(⌒▽⌒)☞"
   final_hash = { "get_meaning":{},              "get_emoticon":{} }
   
@@ -47,33 +46,8 @@ def get_english_meaning(file_path, emoticon)
   
   return result
 
-=======
-              # "☜(⌒▽⌒)☞" => "angel"        "O:)" => "☜(⌒▽⌒)☞"
-  final_hash = { "get_meaning":{}, "get_emoticon":{} }
-  
-  result.each do |meaning ,emoticon|
-          emoticon.each do |japanese_emoticon, english_emoticon|
-           binding.pry
-                if final_hash[:get_meaning][japanese_emoticon]== nil 
-                  final_hash[:get_meaning][japanese_emoticon] =  meaning
-                end
-              
-                if final_hash[:get_emoticon][english_emoticon] == nil 
-                  final_hash[:get_emoticon][english_emoticon] = japanese_emoticon
-                end 
-                
-          end
-  end 
-  final_hash
 end
-
-
-
-def get_japanese_emoticon
-  # code goes here
->>>>>>> cde7537be5c5e4c8a00b5d8351d79e9792bee3ee
-end
-
+learn
 
 
 
